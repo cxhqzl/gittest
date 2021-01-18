27eed808629582187ce1d48ab59193d9b7beaba6
cxhqzl <cxhqzl@users.noreply.github.com>
2021-01-18 09:36:55 +0800

    提交测试 源代码、配置文件、依赖

---
+++ cxhqzl/src/main/java/com/cxh/CxhqzlApplication.java
@@ -0,0 +1,13 @@
+package com.cxh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CxhqzlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CxhqzlApplication.class, args);
+	}
+
+}
